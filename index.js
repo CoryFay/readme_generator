26a2d7e2113c75b908355d1e05f4f3a10348d815
Cory <coryfay97@gmail.com>
2020-12-09 17:31:29 -0800

    remove old .js x2, create template for README

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-// array of questions for user
+
 const questions = [
     {
         type: 'input',
@@ -17,67 +17,97 @@ const questions = [
         message: 'Please explain how to install your project.',
         name: 'install'
     },
-    // {
-    //     type: 'input',
-    //     message: 'Please explain how to use your project.',
-    //     name: 'usage'
-    // },
-    // {
-    //     type: 'checkbox',
-    //     message: 'Please select which type of license you used.',
-    //     choices: ['MIT', 'ISC', 'Apache License 2.0', 'GNU GPLv3'],
-    //     name: 'license'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Please explain your contribution guidelines.',
-    //     name: 'contributions'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Please explain how to run tests for your project.',
-    //     name: 'tests'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Please enter your Github Username.',
-    //     name: 'username'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Please enter your e-mail address.',
-    //     name: 'eMail'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Please enter instructions on how you might be contacted.',
-    //     name: 'contactInfo'
-    // },
+    {
+        type: 'input',
+        message: 'Please explain how to use your project.',
+        name: 'usage'
+    },
+    {
+        type: 'checkbox',
+        message: 'Please select which type of license you used.',
+        choices: ['MIT', 'ISC', 'Apache License 2.0', 'GNU GPLv3'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Please explain your contribution guidelines.',
+        name: 'contributions'
+    },
+    {
+        type: 'input',
+        message: 'Please explain how to run tests for your project.',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your Github Username.',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your e-mail address.',
+        name: 'eMail'
+    },
+    {
+        type: 'input',
+        message: 'Please enter instructions on how you might be contacted.',
+        name: 'contactInfo'
+    },
 ];
-let emptyData;
 
-// function to write README file
-function writeToFile(fileName, emptyData) { 
-    console.log(emptyData); 
-}
-
-// function to initialize program
 function init() {
     inquirer.prompt(questions)
-    .then(data => {
-        emptyData = data;
-    });
-}
+        .then(({
+            projName,
+            description,
+            install,
+            usage,
+            license,
+            contributions,
+            tests,
+            contactInfo,
+            username,
+            eMail
 
-// function to generate markdown for README
-// function generateMarkdown(data) {
-//     return `# ${data.projName}
-  
-//   `;
-  
-//   //writeToFile();
-//   }
+        }) => {
+            const template =
+                ` # ${projName}
+                
+                # Table of Contents
+                [Description](description)
+                [Installation](install)
+                [Usage](usage)
+                [License](license)
+                [Contributions](contributions)
+                [Tests](tests)
+                [Contact Information](contactInfo)
 
-// function call to initialize program
+                ## Description
+                ![License: ${license}](https://img.shields.io/badge/License-${license}-hotpink.svg)
+                ${description}
+
+                ## Installation
+                ${install}
+
+                ## Usage
+                ${usage}
+
+                ## License
+                ${license}
+
+                ## Contributions
+                ${contributions}
+
+                ## Tests
+                ${tests}
+
+                ## Contact Information
+                ${contactInfo}
+                ${username}
+                ${eMail}
+            `
+        }
+        )
+    }
+
 init();
-writeToFile();
